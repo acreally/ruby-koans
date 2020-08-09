23f502252f40d3c9a1bba61e474348cf74db0ff0
Andrew Creally <creallya@gmail.com>
2020-08-09 15:56:57 -0400

    Completed about triangle project 2.

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,22 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  if a <=0 || b <= 0 || c <= 0
+    raise TriangleError, "lengths must be positive values"
+  end
+
+  if b + c <= a
+    raise TriangleError, "Length of side a must more more than lenghts of sides b and c combined."
+  end
+
+  if a + c <= b
+    raise TriangleError, "Length of side b must more more than lenghts of sides a and c combined."
+  end
+
+  if a + b <= c
+    raise TriangleError, "Length of side c must more more than lenghts of sides a and b combined."
+  end
+
   if a == b
     if b == c
       return :equilateral
